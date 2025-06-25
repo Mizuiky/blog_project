f7fcd68ccfd41d9a651312838c04cdff067e96b8
Priscila <priscila.mizuiky@gmail.com>
2025-06-25 12:50:34 -0300

    add test component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-//isso é um componente, quando é page colocamos o nome page no final
-//isso é nossa rota barra / cada rota é a combinacao do nome da pasta + arquivo page.tsx
+import { Header } from "@/components/Header";
 
-import clsx from "clsx";
-
-export default function HomePage() {
+export default async function HomePage() {
+  console.log("Home page");
   return (
-    <div
-      className={clsx(
-        "text-3xl",
-        "font-bold",
-        "text-blue-500",
-        "hover:text-blue-100",
-        "hover:bg-blue-500",
-        "transition",
-        "duration-200"
-      )}
-    >
-      <h1> Ola de dentro do page.tsx</h1>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import clsx from "clsx";
+
+export function Header() {
+  console.log("Header");
+  return (
+    <h1
+      className={clsx(
+        "text-3xl",
+        "font-bold",
+        "text-blue-500",
+        "hover:text-blue-100",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-200"
+      )}
+      onClick={() => alert(123)}
+    >
+      Texto do meu Header
+    </h1>
+  );
+}
