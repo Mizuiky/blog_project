da2ea508681912470ffdfdb3db3420a855fe648f
Priscila <priscila.mizuiky@gmail.com>
2025-06-25 13:02:46 -0300

    add comments

--- src/repositories/post/index.tsx
+++ src/repositories/post/index.tsx
@@ -2,5 +2,7 @@ import { JsonPostRepository } from "./json-post-repository";
 import { PostRepository } from "./post-repository";
 
 //estou dizendo que ele é do tipo post repository da minha interface entao somente os metodos de lá serao acessiveis
-//nao importa se existir mais metodos aqui nesta classe o que importa é o da interface
+//nao importa a classe, ira sempre usar os mesmos metodos da interface
+//A classe pode ser diferente poderem tera implementacoes diferentes para base de dados diferentes porem com a mesma interface
+//Entao eu posso mudar onde esta o new por exmplo para trocar  base de dados
 export const postRepository: PostRepository = new JsonPostRepository();
